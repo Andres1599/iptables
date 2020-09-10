bd6c1dec1995c9db9b5a68aa37b09eb8ba7ea694
Andres Higueros <higueros71@gmail.com>
2020-09-10 14:10:25 -0600

    add: agregando base del API con el framework de express

---
+++ src/app.js
@@ -0,0 +1,33 @@
+const config = require('./configuration/configuration')
+const cors = require('cors')
+const morgan = require('morgan')
+const express = require('express')
+
+var app = express();
+
+let port;
+
+if (config.env != 'DEV') {
+    port = config.port;
+} else {
+    port = 3000
+}
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    next();
+});
+
+app.use(cors());
+app.use('/api/', require('./router')(app));
+
+app.listen(port, () => {
+    console.log("Servidor iniciado en el puerto: " + port);
+    console.log("Debug del server: ");
+});
+
+module.exports = app
